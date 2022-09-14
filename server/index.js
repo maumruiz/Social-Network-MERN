07c03b9972d79c41c52674eddc759da4fbc06182
maumruiz <mau.mruiz@gmail.com>
2022-09-14 13:46:54 -0500

    Create appbar and form components

--- server/index.js
+++ server/index.js
@@ -3,8 +3,22 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
+import postRoutes from './routes/posts.js'
+
 const app = express()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
+app.use('/posts', postRoutes)
+
+const CONNECTION_URL = "mongodb+srv://maindeveloper:happy2code@cluster0.yuiy1fa.mongodb.net/?retryWrites=true&w=majority"
+
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.set('useFindAndModify', false);
